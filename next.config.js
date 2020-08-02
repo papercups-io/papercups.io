9ba7909627d756abbaac20285358f17d8d223581
reichert621 <reichertjalex@gmail.com>
2020-08-02 17:41:37 -0400

    Add emoji support

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 // @ts-check
 
 const rehypePrism = require('@mapbox/rehype-prism');
+const emoji = require('remark-emoji');
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
   options: {
+    remarkPlugins: [emoji],
     rehypePlugins: [rehypePrism],
   },
 });
