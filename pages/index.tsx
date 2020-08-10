335e2f098caf8f3a836b02c29d44ca2a67802981
cheeseblubber <kamleung911@gmail.com>
2020-08-09 19:52:15 -0700

    add google analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import {Box, Flex} from 'rebass';
 import ChatWidget from '@papercups-io/chat-widget';
+
 import {RightCircleOutlined, GithubOutlined} from '../components/icons';
 import {
   colors,
@@ -15,73 +16,86 @@ import {
   Title,
   Text,
 } from '../components/common';
+import ReactGA from 'react-ga';
+import {Component} from 'react';
 
-export default function Home() {
-  return (
-    <Layout style={{background: colors.white}}>
-      <Head>
-        <title>Papercups</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class extends Component {
+  componentDidMount() {
+    const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+    if (NEXT_PUBLIC_GA_TRACKING_ID) {
+      ReactGA.initialize(NEXT_PUBLIC_GA_TRACKING_ID);
+      ReactGA.pageview(document.location.pathname);
+    }
+  }
+  render() {
+    return (
+      <Layout style={{background: colors.white}}>
+        <Head>
+          <title>Papercups</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Header style={{background: 'transparent'}}>
-        <Flex>
-          <Box flex={1}>
-            <Link href="/">
-              <a>
-                <img src="papercups-v2.svg" style={{height: 50, width: 100}} />
-              </a>
-            </Link>
-          </Box>
-
-          <Box>
-            <Menu style={{borderBottom: 'none'}} mode="horizontal">
-              <Menu.Item style={{margin: '0 1em'}} key="home">
-                <Link href="/">
-                  <a>Home</a>
-                </Link>
-              </Menu.Item>
-              <Menu.Item style={{margin: '0 1em'}} key="github">
-                <a
-                  href="https://github.com/papercups-io/papercups"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Github
+        <Header style={{background: 'transparent'}}>
+          <Flex>
+            <Box flex={1}>
+              <Link href="/">
+                <a>
+                  <img
+                    src="papercups-v2.svg"
+                    style={{height: 50, width: 100}}
+                  />
                 </a>
-              </Menu.Item>
-              <Menu.Item style={{margin: '0 1em'}} key="Sign up">
-                <a
-                  href="https://app.papercups.io"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Login
-                </a>
-              </Menu.Item>
-            </Menu>
-          </Box>
-        </Flex>
-      </Header>
+              </Link>
+            </Box>
 
-      <Content>
-        <Box mx="auto" style={{maxWidth: 960}} py={5} px={4}>
-          <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
-            <Box pt={100} flex={1} mx={[0, 4]} mb={[4, 0]}>
-              <Box mb={6}>
-                <Title level={1}>
-                  Connect with your customers. Own your data.
-                </Title>
+            <Box>
+              <Menu style={{borderBottom: 'none'}} mode="horizontal">
+                <Menu.Item style={{margin: '0 1em'}} key="home">
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item style={{margin: '0 1em'}} key="github">
+                  <a
+                    href="https://github.com/papercups-io/papercups"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github
+                  </a>
+                </Menu.Item>
+                <Menu.Item style={{margin: '0 1em'}} key="Sign up">
+                  <a
+                    href="https://app.papercups.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Login
+                  </a>
+                </Menu.Item>
+              </Menu>
+            </Box>
+          </Flex>
+        </Header>
 
-                <Paragraph>
-                  Open source customer messaging, built for startups and
-                  enterprise alike.
-                </Paragraph>
+        <Content>
+          <Box mx="auto" style={{maxWidth: 960}} py={5} px={4}>
+            <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
+              <Box pt={100} flex={1} mx={[0, 4]} mb={[4, 0]}>
+                <Box mb={6}>
+                  <Title level={1}>
+                    Connect with your customers. Own your data.
+                  </Title>
 
-                <Flex my={3}>
-                  <Box mr={2}>
-                    <a
-                      href="https://github.com/papercups-io/papercups"
+                  <Paragraph>
+                    Open source customer messaging, built for startups and
+                    enterprise alike.
+                  </Paragraph>
+
+                  <Flex my={3}>
+                    <Box mr={2}>
+                      <a
+                        href="https://github.com/papercups-io/papercups"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -97,173 +111,177 @@ export default function Home() {
                   <Box mr={2}>
                     <a
                       href="https://app.papercups.io/register"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button
-                        type="primary"
-                        size="large"
-                        icon={<RightCircleOutlined />}
+                        target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        Get started for free
-                      </Button>
-                    </a>
-                  </Box>
+                        <Button
+                          type="primary"
+                          size="large"
+                          icon={<RightCircleOutlined />}
+                        >
+                          Get started for free
+                        </Button>
+                      </a>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Box>
+
+              <Box flex={1} mx={[0, 4]}>
+                <Flex
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{
+                    width: '100%',
+                    height: '100%',
+
+                  }}
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      minHeight: 320,
+                    }}
+                    src="chatting.svg"
+                  />
                 </Flex>
               </Box>
-            </Box>
-
-            <Box flex={1} mx={[0, 4]}>
-              <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <img
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    minHeight: 320,
-                  }}
-                  src="chatting.svg"
-                />
-              </Flex>
-            </Box>
-          </Flex>
-
-          <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
-            <Box flex={1} mx={[0, 4]} mb={[4, 0]}>
-              <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <img
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    minHeight: 320,
-                  }}
-                  src="setup.svg"
-                />
-              </Flex>
-            </Box>
-
-            <Box flex={1} mx={[0, 4]} my={[4, 0]}>
-              <Title level={3}>Chat directly with your users</Title>
-              <Paragraph>
-                Convert more website visitors into customers with real-time
-                chat. Don't lose out on prospects because of unanswered
-                questions!
-              </Paragraph>
-            </Box>
-          </Flex>
-
-          <Flex
-            mb={[5, 6]}
-            mx={[0, -4]}
-            flexDirection={['column-reverse', 'row']}
-          >
-            <Box flex={1} mx={[0, 4]} mb={[4, 0]}>
-              <Title level={3}>Highly customizable widget</Title>
-              <Paragraph>
-                Customize it as much as you'd like to match your existing
-                designs. We also provide a React component for easy
-                customization.
-              </Paragraph>
-            </Box>
-
-            <Box flex={1} mx={[0, 4]}>
-              <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <img
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    minHeight: 320,
-                  }}
-                  src="customize.svg"
-                />
-              </Flex>
-            </Box>
-          </Flex>
-          <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
-            <Box flex={1} mx={[0, 4]}>
-              <Flex
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                <img
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    minHeight: 320,
-                  }}
-                  src="secure.svg"
-                />
-              </Flex>
-            </Box>
-            <Box pt={100} flex={1} mx={[0, 4]} mb={[4, 0]}>
-              <Title level={3}>Integrate with your favorite tools</Title>
-              <Paragraph>
-                Connect Papercups to Slack, Gmail, and many more of your
-                favorite tools. Do you want to connect to a platform we don't
-                currently support? Get in touch!
-              </Paragraph>
-            </Box>
-          </Flex>
-
-          <Flex justifyContent="center" mb={[4, 5]} mt={[6, 0]}>
-            <Title level={2}>Papercups for Enterprise</Title>
-          </Flex>
-
-          <Flex
-            mx={-3}
-            justifyContent="space-between"
-            mb={6}
-            flexDirection={['column', 'row']}
-          >
-            <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
-              <Title level={3}>Self-managed</Title>
-              <Paragraph>
-                Papercups can be deployed in your cloud, for painless adoption
-                and onboarding. Whether it's AWS, Docker, or Heroku, we've got
-                you covered.
-              </Paragraph>
             </Flex>
-            <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
-              <Title level={3}>Unlimited volume</Title>
-              <Paragraph>
-                Papercups is built with Elixir on top of BEAM for incredible
-                scalability. This scalability extends to our open core pricing
-                model.
-              </Paragraph>
+
+            <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
+              <Box flex={1} mx={[0, 4]} mb={[4, 0]}>
+                <Flex
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{
+
+                    width: '100%',
+                    height: '100%',
+                  }}
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      minHeight: 320,
+                    }}
+                    src="setup.svg"
+                  />
+                </Flex>
+              </Box>
+
+              <Box flex={1} mx={[0, 4]} my={[4, 0]}>
+                <Title level={3}>Chat directly with your users</Title>
+                <Paragraph>
+                  Convert more website visitors into customers with real-time
+                  chat. Don't lose out on prospects because of unanswered
+                  questions!
+                </Paragraph>
+              </Box>
             </Flex>
-            <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
-              <Title level={3}>Personalized support</Title>
-              <Paragraph>
-                We can manage your deployment on your infrastructure. Get the
-                benefits of self-hosting with the reliability and scalability of
-                the cloud.
-              </Paragraph>
+
+            <Flex
+              mb={[5, 6]}
+              mx={[0, -4]}
+              flexDirection={['column-reverse', 'row']}
+            >
+              <Box flex={1} mx={[0, 4]} mb={[4, 0]}>
+                <Title level={3}>Highly customizable widget</Title>
+                <Paragraph>
+                  Customize it as much as you'd like to match your existing
+                  designs. We also provide a React component for easy
+                  customization.
+                </Paragraph>
+              </Box>
+
+              <Box flex={1} mx={[0, 4]}>
+                <Flex
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{
+
+                    width: '100%',
+                    height: '100%',
+                  }}
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      minHeight: 320,
+                    }}
+                    src="customize.svg"
+                  />
+                </Flex>
+              </Box>
             </Flex>
-          </Flex>
+            <Flex mb={[5, 6]} mx={[0, -4]} flexDirection={['column', 'row']}>
+              <Box flex={1} mx={[0, 4]}>
+                <Flex
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{
+
+                    width: '100%',
+                    height: '100%',
+                  }}
+                >
+                  <img
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      minHeight: 320,
+                    }}
+                    src="secure.svg"
+                  />
+                </Flex>
+              </Box>
+              <Box pt={100} flex={1} mx={[0, 4]} mb={[4, 0]}>
+                <Title level={3}>Integrate with your favorite tools</Title>
+                <Paragraph>
+                  Connect Papercups to Slack, Gmail, and many more of your
+                  favorite tools. Do you want to connect to a platform we don't
+                  currently support? Get in touch!
+                </Paragraph>
+              </Box>
+            </Flex>
+
+            <Flex justifyContent="center" mb={[4, 5]} mt={[6, 0]}>
+              <Title level={2}>Papercups for Enterprise</Title>
+            </Flex>
+
+            <Flex
+              mx={-3}
+              justifyContent="space-between"
+              mb={6}
+              flexDirection={['column', 'row']}
+            >
+              <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
+                <Title level={3}>Self-managed</Title>
+                <Paragraph>
+                  Papercups can be deployed in your cloud, for painless adoption
+                  and onboarding. Whether it's AWS, Docker, or Heroku, we've got
+                  you covered.
+                </Paragraph>
+              </Flex>
+              <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
+                <Title level={3}>Unlimited volume</Title>
+                <Paragraph>
+                  Papercups is built with Elixir on top of BEAM for incredible
+                  scalability. This scalability extends to our open core pricing
+                  model.
+                </Paragraph>
+              </Flex>
+              <Flex flex={1} mx={3} my={[3, 0]} flexDirection="column">
+                <Title level={3}>Personalized support</Title>
+                <Paragraph>
+                  We can manage your deployment on your infrastructure. Get the
+                  benefits of self-hosting with the reliability and scalability
+                  of the cloud.
+                </Paragraph>
+              </Flex>
+            </Flex>
 
           <Flex justifyContent="center" mb={[3, 4]} mt={[6, 0]}>
             <Title level={2}>Connect with us</Title>
@@ -304,41 +322,49 @@ export default function Home() {
             </Flex>
           </Flex>
 
-          <Flex
-            mb={5}
-            justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
-          >
-            <Title level={1}>What's new?</Title>
+            <Flex
+              mb={5}
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+            >
+              <Title level={1}>What's new?</Title>
 
-            <Box my={4}>
-              <a
-                href="https://github.com/papercups-io/papercups"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button size="large">Find out on Github</Button>
-              </a>
-            </Box>
-          </Flex>
-        </Box>
-      </Content>
+              <Box my={4}>
+                <a
+                  href="https://github.com/papercups-io/papercups"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button size="large">Find out on Github</Button>
+                </a>
+              </Box>
+            </Flex>
+          </Box>
+        </Content>
 
-      <Footer style={{backgroundColor: '#001529'}}>
-        <Box p={5}>
-          <Text style={{color: colors.white}}>
-            Made with ❤️ in SF &amp; NYC
-          </Text>
-        </Box>
-      </Footer>
+        <ChatWidget
+          title="Welcome to Papercups!"
+          subtitle="Ask us anything in the chat window below 😊"
+          primaryColor="#13c2c2"
+          accountId="eb504736-0f20-4978-98ff-1a82ae60b266"
+        />
 
-      <ChatWidget
-        title="Welcome to Papercups!"
-        subtitle="Ask us anything in the chat window below 😊"
-        primaryColor="#1890ff"
-        accountId="eb504736-0f20-4978-98ff-1a82ae60b266"
-      />
-    </Layout>
-  );
+        <Footer style={{backgroundColor: '#001529'}}>
+          <Box p={5}>
+            <Text style={{color: colors.white}}>
+              Made with ❤️ in SF &amp; NYC
+            </Text>
+          </Box>
+        </Footer>
+
+        <ChatWidget
+          title="Welcome to Papercups!"
+          subtitle="Ask us anything in the chat window below 😊"
+          primaryColor="#1890ff"
+          accountId="eb504736-0f20-4978-98ff-1a82ae60b266"
+        />
+      </Layout>
+    );
+  }
 }
