7d3b10f1dc49866e3044a8ea91addd47c1c8eb73
reichert621 <reichertjalex@gmail.com>
2021-09-02 14:33:02 -0400

    Temporarily remove ChatDemo from landing page to prevent auto focus

--- pages/index.tsx
+++ pages/index.tsx
@@ -299,7 +299,15 @@ export default class extends Component {
                     height: '100%',
                   }}
                 >
-                  <ChatDemo />
+                  <Image
+                    sx={{
+                      width: '100%',
+                      height: '100%',
+                      minHeight: 320,
+                    }}
+                    src="customize.gif"
+                  />
+                  {/* <ChatDemo /> */}
                 </Flex>
               </Box>
             </Flex>
